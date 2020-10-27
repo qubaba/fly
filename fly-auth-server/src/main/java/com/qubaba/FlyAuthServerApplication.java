cefb4d7c1fa03814acdf278bd7ab3393581bdd2a
qubaba <2055294353@qq.com>
2020-10-27 15:40:39 +0800

    init fly-auth-server

---
+++ fly-auth-server/src/main/java/com/qubaba/FlyAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.qubaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyAuthServerApplication.class, args);
+    }
+
+}
---
+++ fly-auth-server/src/test/java/com/qubaba/FlyAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qubaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
