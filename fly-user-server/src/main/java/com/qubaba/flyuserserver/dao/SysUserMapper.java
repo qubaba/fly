3b7120e489ff61738c557e796eb694b3a04c6cd8
qubaba <2055294353@qq.com>
2021-01-22 16:46:42 +0800

    add lombok

--- fly-user-server/src/main/java/com/qubaba/flyuserserver/dao/SysUserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.qubaba.flyuserserver.dao;
-
-import com.qubaba.flyuserserver.entity.domain.SysUser;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface SysUserMapper extends Mapper<SysUser> {
-}
